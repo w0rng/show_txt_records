fe55e0a44dda74250e873b6f66c278a1e9b8d6c7
w0rng <antonabramov2000@gmail.com>
2022-12-09 00:30:53 +0700

    + error handling

--- src/main.py
+++ src/main.py
@@ -1,16 +1,27 @@
 from aiohttp import web
 from dns.asyncresolver import resolve as dns_resolve
+from dns.resolver import NXDOMAIN, NoAnswer, NoNameservers, Timeout
 
 routers = web.RouteTableDef()
 
 
 @routers.get("/")
 async def index(request: web.Request) -> web.Response:
-    # get domain name from request
     result = ""
 
-    for record in await dns_resolve(request.host, "TXT"):
-        result += record.strings[0].decode() + "\n"
+    try:
+        for record in await dns_resolve(request.host, "TXT"):
+            result += record.strings[0].decode() + "\n\n"
+    except NoAnswer:
+        result = "No TXT record found"
+    except NXDOMAIN:
+        result = "No such domain"
+    except NoNameservers:
+        result = "No nameservers found"
+    except Timeout:
+        result = "Timeout"
+    except:
+        result = "Unknown error"
     return web.Response(content_type="text/plain", text=result)
 
 
