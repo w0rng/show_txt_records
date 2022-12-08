2a39dc862caec18dcf28f55b9fba8e6d160020b2
w0rng <antonabramov2000@gmail.com>
2022-12-09 01:24:08 +0700

    + cache

--- src/main.py
+++ src/main.py
@@ -1,17 +1,19 @@
 from aiohttp import web
+from asyncache import cached
+from cachetools import TTLCache
 from dns.asyncresolver import resolve as dns_resolve
 from dns.resolver import NXDOMAIN, NoAnswer, NoNameservers, Timeout
 
 routers = web.RouteTableDef()
 
 
-@routers.get("/")
-async def index(request: web.Request) -> web.Response:
-    result = ""
+@cached(cache=TTLCache(maxsize=1024, ttl=600))
+async def resolve(domain: str) -> str:
+    answers = []
 
     try:
-        for record in await dns_resolve(request.host, "TXT"):
-            result += record.strings[0].decode() + "\n\n"
+        for record in await dns_resolve(domain, "TXT"):
+            answers.append(record.strings[0].decode())
     except NoAnswer:
         result = "No TXT record found"
     except NXDOMAIN:
@@ -22,7 +24,16 @@ async def index(request: web.Request) -> web.Response:
         result = "Timeout"
     except:
         result = "Unknown error"
-    return web.Response(content_type="text/plain", text=result)
+    else:
+        result = "\n\n".join(sorted(answers))
+
+    return result
+
+
+@routers.get("/")
+async def index(request: web.Request) -> web.Response:
+    domain = request.query.get("domain")
+    return web.Response(content_type="text/plain", text=await resolve(domain))
 
 
 app = web.Application()
